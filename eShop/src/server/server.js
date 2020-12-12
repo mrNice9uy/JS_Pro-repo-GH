ce65358eec87bcb74b90b5a4c9255faa92cf3eca
geekbrains <info@geekbrains.ru>
2020-12-12 03:38:11 +0300

    JS_Pro-lesson_8-homework

---
+++ eShop/src/server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const cartRouter = require('./cartRouter');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use('/', express.static(path.resolve(__dirname, '../public')));
+app.use('/api/cart', cartRouter);
+
+const catalogJSONPath = path.resolve(__dirname, './db/products.json');
+
+app.get('/api/products', (req, res) => {
+    fs.readFile(catalogJSONPath, 'utf-8', (err, data) => {
+        if (err) {
+            res.send(JSON.stringify({result: 0, text: err}));
+            // res.sendStatus(404, JSON.stringify({result: 0, text: err}));
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+});
